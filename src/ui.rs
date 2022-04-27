8569f7fb176084e3a9ce9829e4991def7f022044
Gregory Szorc <gregory.szorc@gmail.com>
2022-04-27 13:51:45 -0700

    ui: add an obj-c callback mechanism
    This will allow us to install Rust functions as target functions for
    menu items.

--- src/ui.rs
+++ src/ui.rs
@@ -30,7 +30,14 @@ use {
         base::{id, nil, NO, YES},
     },
     cocoa_foundation::foundation::{NSAutoreleasePool, NSData, NSSize, NSString},
-    objc::{msg_send, runtime::Sel, sel, sel_impl},
+    objc::{
+        declare::ClassDecl,
+        msg_send,
+        runtime::{Class, Object, Sel},
+        sel, sel_impl, Message,
+    },
+    objc_foundation::{INSObject, NSObject},
+    objc_id::Id,
     std::{ffi::c_void, ptr::null},
     winit::platform::macos::{ActivationPolicy, EventLoopBuilderExtMacOS},
 };
@@ -700,4 +707,73 @@ pub fn run_app(
             _ => (),
         }
     });
+}
+
+// Mostly copied from tray-item-rs.
+
+pub(crate) enum Callback {}
+unsafe impl Message for Callback {}
+
+pub(crate) struct CallbackState {
+    cb: Box<dyn Fn() -> ()>,
+}
+
+impl Callback {
+    pub(crate) fn from(cb: Box<dyn Fn() -> ()>) -> Id<Self> {
+        let cbs = CallbackState { cb };
+        let bcbs = Box::new(cbs);
+
+        let ptr = Box::into_raw(bcbs);
+        let ptr = ptr as *mut c_void as usize;
+        let mut oid = <Callback as INSObject>::new();
+        (*oid).setptr(ptr);
+        oid
+    }
+
+    pub(crate) fn setptr(&mut self, uptr: usize) {
+        unsafe {
+            let obj = &mut *(self as *mut _ as *mut ::objc::runtime::Object);
+            obj.set_ivar("_cbptr", uptr);
+        }
+    }
+}
+
+// TODO: Drop for $name doesn't get called, probably because objc manages the memory and
+// releases it for us.  so we leak the boxed callback right now.
+
+impl INSObject for Callback {
+    fn class() -> &'static Class {
+        let cname = "Callback";
+
+        let mut klass = Class::get(cname);
+        if klass.is_none() {
+            let superclass = NSObject::class();
+            let mut decl = ClassDecl::new(&cname, superclass).unwrap();
+            decl.add_ivar::<usize>("_cbptr");
+
+            extern "C" fn sysbar_callback_call(this: &Object, _cmd: Sel) {
+                unsafe {
+                    let pval: usize = *this.get_ivar("_cbptr");
+                    let ptr = pval as *mut c_void;
+                    let ptr = ptr as *mut CallbackState;
+                    let bcbs: Box<CallbackState> = Box::from_raw(ptr);
+                    {
+                        (*bcbs.cb)();
+                    }
+                    std::mem::forget(bcbs);
+                }
+            }
+
+            unsafe {
+                decl.add_method(
+                    sel!(call),
+                    sysbar_callback_call as extern "C" fn(&Object, Sel),
+                );
+            }
+
+            decl.register();
+            klass = Class::get(cname);
+        }
+        klass.unwrap()
+    }
 }
